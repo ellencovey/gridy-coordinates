29fe4f0839000ba4c93fd82a2e93c6114d089201
Ellen Covey <covey.ellen@gmail.com>
2019-07-29 14:33:47 +0100

    Rewrote slice function to produce flattened array in order

--- Gridy/Controller/PuzzleViewController.swift
+++ Gridy/Controller/PuzzleViewController.swift
@@ -13,35 +13,65 @@ class PuzzleViewController: UIViewController, UICollectionViewDataSource, UIColl
     @IBOutlet weak var collectionView: UICollectionView!
     
     var editedImage = UIImage(named: "pic-ben") // placeholder image for screenshot from last view
-    var tileArr = [[UIImage]]() // will contain small pieces of image
-    lazy var tileImages = tileArr.reduce([], +) // this will pass to collection view
+//    var tileArr = [[UIImage]]() // will contain small pieces of image
+//    lazy var tileImages = tileArr.reduce([], +) // this will pass to collection view
     
-    // to do - split into single dictionary Int & UIImage, not array of arrays
-    func splitImage(row : Int , column : Int) {
-        
+    // testing
+    var finalArr = [UIImage]() // equivalent of tileArr for this function
+    lazy var shuffledArr = finalArr.shuffled()
+    
+    func sliceImage(gridSize: Int) {
         let originalImg = editedImage
         
-        let tileHeight = (editedImage?.size.height)! / CGFloat (row)
-        let tileWidth = (editedImage?.size.width)! / CGFloat (column)
+        let tileHeight = (editedImage?.size.height)! / CGFloat (gridSize)
+        let tileWidth = (editedImage?.size.width)! / CGFloat (gridSize)
         let scale = (editedImage?.scale)!
         
-        for y in 0 ..< row {
+        for y in 0 ..< gridSize {
             var yArr = [UIImage]()
-            for x in 0 ..< column {
+            for x in 0 ..< gridSize {
                 UIGraphicsBeginImageContextWithOptions(CGSize(width: tileWidth, height: tileHeight), false, 0) // size, opaque, scale
-                let i =  originalImg?.cgImage?.cropping(to:  CGRect.init(x: CGFloat(x) * tileWidth * scale, y: CGFloat(y) * tileHeight * scale, width: tileWidth * scale, height: tileHeight * scale) )
+                let i = originalImg?.cgImage?.cropping(to: CGRect.init(x: CGFloat(x) * tileWidth * scale, y: CGFloat(y) * tileHeight * scale, width: tileWidth * scale, height: tileHeight * scale))
                 let newImg = UIImage.init(cgImage: i!)
                 
                 yArr.append(newImg)
-                
                 UIGraphicsEndImageContext();
-                
             }
-            let shuffledYArr = yArr.shuffled()
-            tileArr.append(shuffledYArr)
+            
+            for tile in yArr {
+                finalArr.append(tile)
+            }
+            
         }
         
     }
+    
+//    func splitImage(row : Int , column : Int) {
+//
+//        let originalImg = editedImage
+//
+//        let tileHeight = (editedImage?.size.height)! / CGFloat (row)
+//        let tileWidth = (editedImage?.size.width)! / CGFloat (column)
+//        let scale = (editedImage?.scale)!
+//
+//        for y in 0 ..< row {
+//            var yArr = [UIImage]()
+//            for x in 0 ..< column {
+//                UIGraphicsBeginImageContextWithOptions(CGSize(width: tileWidth, height: tileHeight), false, 0) // size, opaque, scale
+//                let i =  originalImg?.cgImage?.cropping(to:  CGRect.init(x: CGFloat(x) * tileWidth * scale, y: CGFloat(y) * tileHeight * scale, width: tileWidth * scale, height: tileHeight * scale) )
+//                let newImg = UIImage.init(cgImage: i!)
+//
+//                yArr.append(newImg)
+//
+//                UIGraphicsEndImageContext();
+//
+//            }
+//            let shuffledYArr = yArr.shuffled()
+//            tileArr.append(shuffledYArr)
+//        }
+//
+//    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -50,17 +80,17 @@ class PuzzleViewController: UIViewController, UICollectionViewDataSource, UIColl
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        splitImage(row: 3, column: 3)
+        sliceImage(gridSize: 3)
     }
     
     // configuring collection view
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return tileImages.count
+        return shuffledArr.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
-        cell.tileImageView.image = tileImages[indexPath.item]
+        cell.tileImageView.image = shuffledArr[indexPath.item]
         return cell
     }
     
